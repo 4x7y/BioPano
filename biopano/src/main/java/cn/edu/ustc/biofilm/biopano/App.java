401ed0c619e5bbb9e797e3d0d62bfe3d2ee56f02
Yuechuan Xue <xueyuechuan@gmail.com>
2015-02-10 19:39:21 +0800

    setup as a maven project

---
+++ biopano/src/main/java/cn/edu/ustc/biofilm/biopano/App.java
@@ -0,0 +1,13 @@
+package cn.edu.ustc.biofilm.biopano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
